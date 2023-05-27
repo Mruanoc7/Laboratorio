99c8d86ccca23a887374ed70f490e481d08e31dd
mruano03 <107089598+mruano03@users.noreply.github.com>
2023-05-27 12:44:16 -0600

    Agregar archivos de proyecto.

---
+++ Laboratorio/CLASES/EMPLEADO.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio.CLASES
+{
+    public class EMPLEADO
+    {
+        private int v1;
+        private string v2;
+        private string v3;
+        private int v4;
+        private string v5;
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+
+        public int Edad { get; set; }
+        public string Cargo { get; set; }
+
+        public decimal Sueldo { get; set; }
+
+
+        public EMPLEADO(int id, string nombre, string apellido, int edad, string cargo, decimal sueldo)
+        {
+            Id = id;
+            Nombre = nombre;
+            this.Apellido = apellido;
+            Edad = edad;
+            Cargo = cargo;
+            Sueldo = sueldo;
+        }
+
+        public EMPLEADO(int v1, string v2, string v3, int v4, string v5)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+            this.v4 = v4;
+            this.v5 = v5;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id} Nombre: {Nombre} Apellido: {Apellido} Edad: {Edad} Cargo: {Cargo} Sueldo: {Sueldo}";
+        }
+    }
+}
---
+++ Laboratorio/Program.cs
@@ -0,0 +1,38 @@
+﻿using Laboratorio.CLASES;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+   
+        string Path = "C:\\Users\\Mynor Ruano\\source\\repos\\Laboratorio\\Empleados4.db";
+        GestorEmpleados gestorEmpleados = new GestorEmpleados(Path);
+
+
+        gestorEmpleados.CrearTabla();
+
+
+        gestorEmpleados.AgregarEmpleado(new EMPLEADO(1, "Juan", "Pérez", 30, "Desarrollador", 25000));
+        gestorEmpleados.AgregarEmpleado(new EMPLEADO(2, "María", "López", 25, "Diseñador", 22000));
+        gestorEmpleados.AgregarEmpleado(new EMPLEADO(3, "Carlos", "González", 35, "Gerente"));
+        gestorEmpleados.AgregarEmpleado(new EMPLEADO(4, "Laura", "Martínez", 28, "Analista", 28000));
+        gestorEmpleados.AgregarEmpleado(new EMPLEADO(5, "Pedro", "Rodríguez", 32, "Desarrollador", 25000));
+        gestorEmpleados.AgregarEmpleado(new EMPLEADO(6, "Ana", "Hernández", 27, "Diseñador", 22000));
+        gestorEmpleados.AgregarEmpleado(new EMPLEADO(7, "Luis", "Sánchez", 29, "Analista", 28000));
+        gestorEmpleados.AgregarEmpleado(new EMPLEADO(8, "Sofía", "Gómez", 31, "Gerente", 50000));
+        gestorEmpleados.AgregarEmpleado(new EMPLEADO(9, "Diego", "Luna", 33, "Desarrollador", 25000));
+        gestorEmpleados.AgregarEmpleado(new EMPLEADO(10, "Isabela", "Vargas", 26, "Diseñador", 22000));
+
+        List<EMPLEADO> empleados = gestorEmpleados.ObtenerEmpleados();
+
+
+        foreach (EMPLEADO empleado in empleados)
+        {
+            Console.WriteLine(empleado);
+        }
+        gestorEmpleados.ObtenerEmpleados().ForEach(e => Console.WriteLine(e.Nombre));
+    }
+}
